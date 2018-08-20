7e40ee63c879a8072af8a61930538ec92584cbcb
songfuxing <songfuxing001@163.com>
2018-08-20 13:05:53 +0800

    virtual proxy

---
+++ src/com/songfuxing/patterns/proxy/virtualProxy/ImageProxy.java
@@ -0,0 +1,57 @@
+package com.songfuxing.patterns.proxy.virtualProxy;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ * Created by songfuxing on 2018/8/20.
+ */
+public class ImageProxy implements Icon {
+    ImageIcon imageIcon;
+    URL imageURL;
+    Thread retrievalThread;
+    boolean retrievaing = false;
+
+    public ImageProxy (URL url) {
+        imageURL = url;
+    }
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        if (imageIcon != null) {
+            imageIcon.paintIcon(c, g, x, y);
+        } else {
+            g.drawString("Loading CD cover. please wait...", x + 300, y + 300);
+            if (!retrievaing) {
+                retrievaing = true;
+                retrievalThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            imageIcon = new ImageIcon(imageURL, "CD Cover");
+                            c.repaint();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                retrievalThread.start();
+            }
+        }
+
+    }
+
+    @Override
+    public int getIconWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getIconHeight() {
+        return 0;
+    }
+}
