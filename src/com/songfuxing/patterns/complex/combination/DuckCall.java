301b99b2032bbd4f8ccd80da1deb86af10a63b7b
songfuxing <songfuxing001@163.com>
2018-08-27 18:54:52 +0800

    duck call

--- src/com/songfuxing/patterns/complex/combination/DuckCall.java
+++ src/com/songfuxing/patterns/complex/combination/DuckCall.java
@@ -1,22 +1,29 @@
 package com.songfuxing.patterns.complex.combination;
 
-import java.util.Observer;
 
 /**
  * 一种鸭叫器
  */
 public class DuckCall implements Quackable {
+    // 被观察者辅助类，用于统一处理observable的资源
+    ObservableProxy observableProxy;
+
+    public DuckCall() {
+        this.observableProxy = new ObservableProxy(this);
+    }
+
     public void quack() {
         System.out.println("duck call: Kwak");
+        notifyObservers();
     }
 
     @Override
     public void registerObserver(Observer observer) {
-
+        observableProxy.registerObserver(observer);
     }
 
     @Override
     public void notifyObservers() {
-
+        observableProxy.notifyObservers();
     }
 }
