74ccc2baabf48da0275fcb2e5449a9055d2df409
songfuxing <songfuxing001@163.com>
2018-08-30 20:30:16 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
+        
     public static void main(String[] args) {
+        int a = 9;
+        String.valueOf(a);
+        StringBuffer s = new StringBuffer();
+        s.append(a);
+        System.out.println(Integer.valueOf(s.toString()));
         System.out.println("Hello World!");
+        System.out.println(Integer.MAX_VALUE );
+        int d = 1534236469;
     }
 }
